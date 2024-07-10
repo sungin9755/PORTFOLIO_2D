c2d2d7652a2d23cf0743d3467479dd58d21a80c2
dsw2679 <dsw2679@naver.com>
2024-07-10 09:35:42 +0900

    초기 커밋 메시지입니다.

---
+++ Source/PORTFOLIO_2D/EnemyZDCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperZDCharacter.h"
+#include "EnemyZDCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTFOLIO_2D_API AEnemyZDCharacter : public APaperZDCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/PORTFOLIO_2D/PlayerZDCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperZDCharacter.h"
+#include "PlayerZDCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTFOLIO_2D_API APlayerZDCharacter : public APaperZDCharacter
+{
+	GENERATED_BODY()
+	
+};
