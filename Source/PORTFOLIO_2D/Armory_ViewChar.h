8c0d8428addf9234fae537ad78060dc04d9a458f
Sungin Cho <sungin9755@gmail.com>
2024-07-11 22:04:30 +0900

    C++ 파일 생성

---
+++ Source/PORTFOLIO_2D/Armory_ViewChar.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidgetBlueprint.h"
+#include "Armory_ViewChar.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTFOLIO_2D_API UArmory_ViewChar : public UUserWidgetBlueprint
+{
+	GENERATED_BODY()
+	
+};
