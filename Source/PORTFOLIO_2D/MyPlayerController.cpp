e22d7ebb06793c3370c490845f42ac728875f9f6
dsw2679 <dsw2679@naver.com>
2024-07-17 08:50:26 +0900

    임시 커밋

--- Source/PORTFOLIO_2D/MyPlayerController.cpp
+++ Source/PORTFOLIO_2D/MyPlayerController.cpp
@@ -8,56 +8,56 @@ AMyPlayerController::AMyPlayerController()
 {
 	bShowMouseCursor = true;
 }
-
-void AMyPlayerController::SetupInputComponent()
-{
-	Super::SetupInputComponent();
-
-	InputComponent->BindAxis("MoveForward", this, &AMyPlayerController::MoveForward);
-	InputComponent->BindAxis("MoveRight", this, &AMyPlayerController::MoveRight);
-}
+//
+//void AMyPlayerController::SetupInputComponent()
+//{
+//	Super::SetupInputComponent();
+//
+//	InputComponent->BindAxis("MoveForward", this, &AMyPlayerController::MoveForward);
+//	InputComponent->BindAxis("MoveRight", this, &AMyPlayerController::MoveRight);
+//}
 
 void AMyPlayerController::PlayerTick(float DeltaTime)
 {
 	Super::PlayerTick(DeltaTime);
 
-	LookMouseCursor();
+	//LookMouseCursor();
 }
 
-void AMyPlayerController::MoveForward(float AxisValue)
-{
-	APawn* const MyPawn = GetPawn();
+//void AMyPlayerController::MoveForward(float AxisValue)
+//{
+//	APawn* const MyPawn = GetPawn();
+//
+//	if (MyPawn)
+//	{
+//		FVector Direction = FVector::ForwardVector;
+//		MyPawn->AddMovementInput(Direction, AxisValue);
+//	}
+//}
+//
+//void AMyPlayerController::MoveRight(float AxisValue)
+//{
+//	APawn* const MyPawn = GetPawn();
+//	if (MyPawn)
+//	{
+//		FVector Direction = FVector::RightVector;
+//		MyPawn->AddMovementInput(Direction, AxisValue);
+//	}
+//}
 
-	if (MyPawn)
-	{
-		FVector Direction = FVector::ForwardVector;
-		MyPawn->AddMovementInput(Direction, AxisValue);
-	}
-}
+//void AMyPlayerController::LookMouseCursor()
+//{
+	//FHitResult Hit;
+	//GetHitResultUnderCursor(ECC_Visibility, false, Hit);
 
-void AMyPlayerController::MoveRight(float AxisValue)
-{
-	APawn* const MyPawn = GetPawn();
-	if (MyPawn)
-	{
-		FVector Direction = FVector::RightVector;
-		MyPawn->AddMovementInput(Direction, AxisValue);
-	}
-}
-
-void AMyPlayerController::LookMouseCursor()
-{
-	FHitResult Hit;
-	GetHitResultUnderCursor(ECC_Visibility, false, Hit);
-
-	if (Hit.bBlockingHit)
-	{
-		APawn* const MyPawn = GetPawn();
-		if (MyPawn)
-		{
-			FRotator LookRotation = UKismetMathLibrary::FindLookAtRotation(MyPawn->GetActorLocation(), FVector(Hit.Location.X, Hit.Location.Y, MyPawn->GetActorLocation().Z));
-			MyPawn->SetActorRotation(LookRotation);
-		}
-	}
-}
+	//if (Hit.bBlockingHit)
+	//{
+	//	APawn* const MyPawn = GetPawn();
+	//	if (MyPawn)
+	//	{
+	//		FRotator LookRotation = UKismetMathLibrary::FindLookAtRotation(MyPawn->GetActorLocation(), FVector(Hit.Location.X, Hit.Location.Y, MyPawn->GetActorLocation().Z));
+	//		MyPawn->SetActorRotation(LookRotation);
+	//	}
+	//}
+//}
 
--- Source/PORTFOLIO_2D/PlayerZDCharacter.cpp
+++ Source/PORTFOLIO_2D/PlayerZDCharacter.cpp
@@ -2,6 +2,9 @@
 
 
 #include "PlayerZDCharacter.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 #include <Components/CapsuleComponent.h>
 #include <Camera/CameraComponent.h>
 #include <GameFramework/SpringArmComponent.h>
@@ -25,24 +28,51 @@ APlayerZDCharacter::APlayerZDCharacter()
 	SpringArmComponent->bInheritPitch = false;
 	SpringArmComponent->bInheritRoll = false;
 	SpringArmComponent->bInheritYaw = false;
-	SpringArmComponent->TargetArmLength = 1200.0f;
-	SpringArmComponent->SetRelativeRotation(FRotator(-60.0f, 0.0f, 0.0f));
+	SpringArmComponent->TargetArmLength = 600.0f;
+	SpringArmComponent->SetRelativeRotation(FRotator(-90.0f, 0.0f, 0.0f));
 	SpringArmComponent->bDoCollisionTest = false;
 
 	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	CameraComponent->SetupAttachment(SpringArmComponent, USpringArmComponent::SocketName);
 	CameraComponent->bUsePawnControlRotation = false;
 
+	//static ConstructorHelpers::FObjectFinder<UInputMappingContext> DEFAULT_CONTEXT(TEXT("/Game/InputAction/IMC_Default.IMC_Default"));
+	//if (DEFAULT_CONTEXT.Succeeded()) DefaultContext = DEFAULT_CONTEXT.Object;
+
+	//static ConstructorHelpers::FObjectFinder<UInputAction> IA_Move(TEXT("/Game/InputAction/IA_Move.IA_Move"));
+	//if (IA_Move.Succeeded()) MoveAction = IA_Move.Object;
+
 }
 
 void APlayerZDCharacter::BeginPlay()
 {
+	Super::BeginPlay();
+
+	//if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	//{
+	//	if (UEnhancedInputLocalPlayerSubsystem* SubSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+	//	{
+	//		SubSystem->AddMappingContext(DefaultContext, 0);
+	//	}
+	//}
 }
 
 void APlayerZDCharacter::Tick(float DeltaTime)
 {
 }
 
-void APlayerZDCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-}
+//void APlayerZDCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+//{
+//	//Super::SetupPlayerInputComponent(PlayerInputComponent);
+//
+//	//if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+//	//{
+//	//	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APlayerZDCharacter::Move);
+//	//}
+//}
+
+//void APlayerZDCharacter::Move(const FInputActionInstance& Instance)
+//{
+//	//FVector2D InputValue = Instance.GetValue().Get<FVector2D>();
+//}
+
